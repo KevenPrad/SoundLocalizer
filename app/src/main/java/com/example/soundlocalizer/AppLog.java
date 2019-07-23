bc45b1ef81ec34f7894c2f8c9a36f8d3851b98fc
KevenPrad <keven.pradeilles@gmail.com>
2019-07-23 09:48:45 +0200

    first commit

---
+++ app/src/main/java/com/example/soundlocalizer/AppLog.java
@@ -0,0 +1,14 @@
+package com.example.soundlocalizer;
+import android.util.Log;
+
+
+public class AppLog {
+
+    final int REQUEST_PERMISSION_CODE=1000;
+
+    private static final String APP_TAG = "AudioRecorder";
+
+    public static int logString(String message){
+        return Log.i(APP_TAG,message);
+    }
+}
